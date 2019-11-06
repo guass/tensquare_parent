17c434ba5c370a5ced4643733f6b97626fcbe6b0
guass <624512733@qq.com>
2019-11-06 23:26:37 +0800

    update

---
+++ tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
@@ -0,0 +1,21 @@
+package com.tensquare.qa;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import utils.IdWorker;
+
+@SpringBootApplication
+@MapperScan(value = "com.tensquare.qa.dao")
+public class QaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker();
+    }
+}
