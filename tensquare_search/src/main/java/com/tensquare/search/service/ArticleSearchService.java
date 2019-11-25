28512791a9bc3bb21c141ecfbcb65c890176ed24
guass <guasszjg@gmail.com>
2019-11-25 19:20:30 +0800

    add api for article es

--- tensquare_search/src/main/java/com/tensquare/search/service/ArticleSearchService.java
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleSearchService.java
@@ -1,11 +1,24 @@
 package com.tensquare.search.service;
 
+import com.tensquare.search.dao.ArticleSearchDao;
+import com.tensquare.search.pojo.Article;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 /**
  * @author: guass
  * @description: 设备管理类
  * @data: 2019-05-28 12:20
  **/
+@Service
 public class ArticleSearchService {
 
+    @Resource
+    private ArticleSearchDao articleSearchDao;
+
+    public void add(Article article){
+        articleSearchDao.save(article);
+    }
 
 }
