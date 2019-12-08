0ed0687efb909e18dc763928b61a86754284e1c0
guass <624512733@qq.com>
2019-12-08 18:41:02 +0800

    update msg

--- tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
@@ -3,11 +3,13 @@ package com.tensquare.qa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import utils.IdWorker;
 
 @SpringBootApplication
 @MapperScan(value = "com.tensquare.qa.dao")
+@EnableDiscoveryClient
 public class QaApplication {
 
     public static void main(String[] args) {
